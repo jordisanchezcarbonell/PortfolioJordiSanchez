56099a7eb611332f221db545d42cf74abfb79c74
Jordi <kazukunpayeer@gmail.com>
2020-07-23 21:02:11 +0200

    Update PersonalInfo.vue

--- src/components/PersonalInfo.vue
+++ src/components/PersonalInfo.vue
@@ -6,7 +6,6 @@
     <div class="info"><strong>Skype:</strong> jordiSanchez</div>
     <div class="info"><strong>Phone Number:</strong> +34676564769 </div>
     <div class="info"><strong>City:</strong> Barcelona </div>
-        <iframe src="https://tierlistgames.h5p.com/content/1291007162358909947/embed" width="1088" height="637" frameborder="0" allowfullscreen="allowfullscreen" allow="geolocation *; microphone *; camera *; midi *; encrypted-media *"></iframe>
 
     <Social/>
   </div>
